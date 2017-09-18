4f68a82673e2cac430a2faaf72863c23af6cb955
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-09-18 18:20:28 -0400

    up to date

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan'),
+    chalk = require('chalk'),
+    lodash = require('lodash');
 const app = express();
 
 app.listen(3000, () => {console.log('YO IM HERE')});
+app.get('/', (request, response) => {
+    response.send('Welcome');
+});
+app.use(morgan('combined'));
